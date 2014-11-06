f3e8d5ff8596ffa77f9461d44ad4293eaa4c2820
Espen Zaal <espenbz@gmail.com>
2014-11-06 09:22:02 +0100

    Lagt inn FlatUI men ikke konfigurert alt. Må fortsette startupguide fra det som har med Bower å gjøre.

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="./css/main.css">
+		<link href="./flatui/dist/css/flat-ui.css" rel="stylesheet">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="./flatui/dist/js/flat-ui.js"></script>
 		<title>TuxMin - SkoleLinux på ny måte!</title>
 	</head>
 	
@@ -13,6 +16,7 @@
 				<header>
 				Header
 				</header>
+				
 				<div id="menu"></div>
 
 
