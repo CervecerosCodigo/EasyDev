b4d23b1f524213631dc01b76e40a2a3e32f03c71
Espen Zaal <espenbz@gmail.com>
2014-11-10 09:53:33 +0100

    Testet toppmeny

--- header.php
+++ header.php
@@ -1,25 +1,76 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="./css/main.css">
-		<link href="./flatui/dist/css/flat-ui.css" rel="stylesheet">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script src="./flatui/dist/js/flat-ui.js"></script>
-		<title>TuxMin - SkoleLinux på ny måte!</title>
-	</head>
-	
-	<body>
-		<div id="mainframe">
-			<div id="allcontent">
-	
-				<header>
-				Header
-				</header>
-				
-				<div id="menu"></div>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link href="./css/main.css" rel="stylesheet" type="text/css">
+<link href="./flatui/dist/css/flat-ui.css" rel="stylesheet">
+<link href="./flatui/dist/css/vendor/bootstrap.min.css" rel="stylesheet">
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="./flatui/dist/js/flat-ui.js"></script>
+
+
+<title>TuxMin - SkoleLinux på ny måte!</title>
+</head>
+
+<body>
+	<div id="mainframe">
+		<div id="allcontent">
+
+			<header>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<nav class="navbar navbar-inverse" role="navigation">
+
+								<div class="collapse navbar-collapse navbar-collapse-01">
+									<ul class="nav navbar-nav navbar-left">
+									
+										<li class="dropdown"><a href="#fakelink" class="dropdown-toggle" data-toggle="dropdown"> Ressurser <span class="caret"></span> </a>
+											<ul class="dropdown-menu">
+												<li><a href="#fakelink">Configure Clubs</a></li>
+												<li><a href="#fakelink">Configure Feature Plans</a></li>
+											</ul> <!-- /Sub menu -->
+										</li>
+										
+										<li class="dropdown"><a href="#fakelink" class="dropdown-toggle" data-toggle="dropdown"> Meldinger <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#fakelink">View Promo List</a></li>
+												<li><a href="#fakelink">Add Edit Promo</a></li>
+												<li><a href="#fakelink">Promo Details</a></li>
+											</ul> <!-- /Sub menu -->
+										</li>
+										
+										<li class="dropdown"><a href="#fakelink" class="dropdown-toggle" data-toggle="dropdown"> Servere <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#fakelink">Analytics</a></li>
+												<li><a href="#fakelink">Sales Report</a></li>
+											</ul> <!-- /Sub menu -->
+										</li>
+										
+										<li class="dropdown"><a href="#fakelink" class="dropdown-toggle" data-toggle="dropdown"> System <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#fakelink">Analytics</a></li>
+												<li><a href="#fakelink">Sales Report</a></li>
+											</ul> <!-- /Sub menu -->
+										</li>
+										
+
+									</ul>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+				<!-- /.container -->
+
+
+			</header>
+
 
 
 <?php
-
 ?>
